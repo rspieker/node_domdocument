201c783818b6e0e69441b1f25d4b7e529ced1c8e
Rogier Spieker <rogier@konfirm.net>
2014-02-16 23:07:08 +0100

    Started work on writing proper tests
    + added mocha as ‘devDependency’ (for testing)
    + added Makefile, so one can run ‘make test’
    + added very basic test.xml, which is actually xhtml 5

---
+++ test/document.js
@@ -0,0 +1,50 @@
+var assert = require('assert'),
+	DOMDocument = require('./../lib/dom.js');
+
+
+suite('DOM', function(){
+
+	test('Document', function(done){
+		var dom = new DOMDocument(),
+			length;
+
+		dom.load(__dirname + '/test.xml', function(error, document){
+
+			assert.equal(document.nodeType, 9);
+			assert.equal(document.nodeName, '#document');
+
+			assert.equal(document.childNodes.length, 2);
+
+			assert.equal(document.firstChild.nodeValue, '  Don\'t get blown away by the apparent XML syntax, HTML5 allows for this  ');
+
+			//  append '*haha*' to the firstChild (#comment)
+			document.firstChild.appendData('*haha*');
+			assert.equal(document.firstChild.nodeValue, '  Don\'t get blown away by the apparent XML syntax, HTML5 allows for this  *haha*');
+
+			//  remove the first 12 characters
+			document.firstChild.deleteData(0, 12);
+			assert.equal(document.firstChild.nodeValue, 'blown away by the apparent XML syntax, HTML5 allows for this  *haha*');
+
+			//  remove more characters than available
+			length = document.firstChild.nodeValue.length;
+			document.firstChild.deleteData(length - 24, 100);
+			assert.equal(document.firstChild.nodeValue, 'blown away by the apparent XML syntax, HTML5');
+
+			//  replace 'XML' with 'XHTML'
+			document.firstChild.replaceData(27, 3, 'XHTML');
+			assert.equal(document.firstChild.nodeValue, 'blown away by the apparent XHTML syntax, HTML5');
+
+			//  obtain the first 10 characters
+			assert.equal(document.firstChild.substringData(0, 10), 'blown away');
+
+			//  obtain the first 10 characters
+			assert.equal(document.firstChild.substringData(41, 10), 'HTML5');
+
+			//  test the data property
+			assert.equal(document.firstChild.nodeValue, document.firstChild.data);
+
+			done();
+		});
+
+	});
+});
