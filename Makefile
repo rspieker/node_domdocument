523eaeb7cac2cda6a66e65d49550c89b5aa72965
Rogier Spieker <rogier@konfirm.net>
2014-10-10 10:59:54 +0200

    Moved unit test from mocha to lab
    package.json
    * changed devDependencies from mocha to lab
    
    lib/sax.js
    * commented out the console.log statements, as we don't want these to show up in the test output
    
    test/document.js
    * ported all tests to use the Lab syntax
    
    test/test.xml
    * added a (valid) entity block, which we should be implementing at a later stage
    
    update Makefile to use lab, added 'test-coverage' and 'test-lint' instructions

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 
 test:
-	@./node_modules/.bin/mocha -u tdd
+	@./node_modules/.bin/lab
+
+test-coverage:
+	@./node_modules/.bin/lab -c
+
+test-lint:
+	@./node_modules/.bin/lab -L
 
 todo:
 	@grep -ir --exclude-dir=node_modules todo * | cut -d: -f2- | tr "\t\/" " " | sed 's/^ *//'
--- test/document.js
+++ test/document.js
@@ -1,68 +1,67 @@
-var assert = require('assert'),
+var Lab = require('lab'),
+	lab = exports.lab = Lab.script(),
 	DOMDocument = require('./../lib/dom.js');
 
-function elapsed(time, prec)
-{
-	var delta = process.hrtime(time);
-	prec = prec || 1e3;
 
-	return Math.round(((delta[0] * 1e3) + (delta[1] / 1e6)) * prec) / prec;
-}
+lab.experiment('Document', function(){
 
-suite('DOM', function(){
+	lab.test('Node existence and typing', function(done){
 
-	test('Document', function(done){
-		var dom = new DOMDocument(),
-			time = process.hrtime(),
-			length;
+		new DOMDocument().load(__dirname + '/test.xml', function(error, document){
+			var length;
 
-		dom.load(__dirname + '/test.xml', function(error, document){
-//			console.log(elapsed(time));
+			Lab.expect(document.nodeType).to.equal(9);
+			Lab.expect(document.nodeName).to.equal('#document');
 
-			assert.equal(document.nodeType, 9);
-			assert.equal(document.nodeName, '#document');
+			//  Basic traversal
+			Lab.expect(document.childNodes.length).to.equal(2);
 
-			assert.equal(document.childNodes.length, 2);
-
-			assert.equal(document.firstChild.nodeValue, '  Don\'t get blown away by the apparent XML syntax, HTML5 allows for this  ');
+			Lab.expect(document.firstChild).to.equal(document.childNodes[0]);
+			Lab.expect(document.firstChild.nodeType).to.equal(8);
+			Lab.expect(document.firstChild.nodeName).to.equal('#comment');
+			Lab.expect(document.firstChild.nodeValue).to.equal('  Don\'t get blown away by the apparent XML syntax, HTML5 allows for this  ');
+			Lab.expect(document.firstChild.nodeValue).to.equal(document.firstChild.data);
 
 			//  append '*haha*' to the firstChild (#comment)
 			document.firstChild.appendData('*haha*');
-			assert.equal(document.firstChild.nodeValue, '  Don\'t get blown away by the apparent XML syntax, HTML5 allows for this  *haha*');
+			Lab.expect(document.firstChild.nodeValue).to.equal('  Don\'t get blown away by the apparent XML syntax, HTML5 allows for this  *haha*');
+			Lab.expect(document.firstChild.nodeValue).to.equal(document.firstChild.data);
 
 			//  remove the first 12 characters
 			document.firstChild.deleteData(0, 12);
-			assert.equal(document.firstChild.nodeValue, 'blown away by the apparent XML syntax, HTML5 allows for this  *haha*');
+			Lab.expect(document.firstChild.nodeValue).to.equal('blown away by the apparent XML syntax, HTML5 allows for this  *haha*');
+			Lab.expect(document.firstChild.nodeValue).to.equal(document.firstChild.data);
 
 			//  remove more characters than available
 			length = document.firstChild.nodeValue.length;
 			document.firstChild.deleteData(length - 24, 100);
-			assert.equal(document.firstChild.nodeValue, 'blown away by the apparent XML syntax, HTML5');
+			Lab.expect(document.firstChild.nodeValue).to.equal('blown away by the apparent XML syntax, HTML5');
+			Lab.expect(document.firstChild.nodeValue).to.equal(document.firstChild.data);
 
 			//  replace 'XML' with 'XHTML'
 			document.firstChild.replaceData(27, 3, 'XHTML');
-			assert.equal(document.firstChild.nodeValue, 'blown away by the apparent XHTML syntax, HTML5');
+			Lab.expect(document.firstChild.nodeValue).to.equal('blown away by the apparent XHTML syntax, HTML5');
+			Lab.expect(document.firstChild.nodeValue).to.equal(document.firstChild.data);
 
 			//  obtain the first 10 characters
-			assert.equal(document.firstChild.substringData(0, 10), 'blown away');
+			Lab.expect(document.firstChild.substringData(0, 10)).to.equal('blown away');
+			Lab.expect(document.firstChild.nodeValue).to.equal(document.firstChild.data);
 
 			//  obtain the last 10 characters starting 5 chars from the end.
 			length = document.firstChild.nodeValue.length;
-			assert.equal(document.firstChild.substringData(length - 5, 10), 'HTML5');
+			Lab.expect(document.firstChild.substringData(length - 5, 10)).to.equal('HTML5');
 
 			//  test the data property
-			assert.equal(document.firstChild.nodeValue, document.firstChild.data);
+			Lab.expect(document.firstChild.nodeValue).to.equal(document.firstChild.data);
 
 			done();
 		});
 
 	});
 
-	test('Multiple Documents', function(done){
-		var domA = new DOMDocument(),
-			domB = new DOMDocument(),
-			docs = [];
 
+	lab.test('Multiple Documents', function(done){
+		var docs = [];
 
 		function test()
 		{
@@ -75,136 +74,131 @@ suite('DOM', function(){
 			{
 				child = a.adoptNode(b.documentElement.firstChild);
 
-				assert.equal(child.ownerDocument, a);
+				Lab.expect(child.ownerDocument).to.equal(a);
 				tmp.appendChild(child);
 			}
-			a.documentElement.appendChild(tmp);
 
 			done();
 		}
 
-		domA.load(__dirname + '/test.xml', function(error, document){
+		new DOMDocument().load(__dirname + '/test.xml', function(error, document){
 			docs.push(document);
 			if (docs.length === 2)
 				test();
 		});
 
-		domB.load(__dirname + '/test.xml', function(error, document){
+		new DOMDocument().load(__dirname + '/test.xml', function(error, document){
 			docs.push(document);
 			if (docs.length === 2)
 				test();
 		});
 	});
 
-	test('Element', function(done){
-		var dom = new DOMDocument(),
-			time = process.hrtime(),
+	lab.test('Element', function(done){
+		var time = process.hrtime(),
 			length;
 
-		dom.load(__dirname + '/test.xml', function(error, document){
-
+		new DOMDocument().load(__dirname + '/test.xml', function(error, document){
 			var strong = document.getElementsByTagName('strong'),
 				a = document.createElement('a'),
 				b = document.createElement('b'),
-				c, bcn;
+				c, bcn, body;
 
-
-			assert.equal(strong.length, 1);
-			assert.equal(strong[0].nodeName, 'strong');
-			assert.equal(strong[0].localName, 'strong');
-			assert.equal(strong[0].prefix, null);
+			Lab.expect(strong.length).to.equal(1);
+			Lab.expect(strong[0].nodeName).to.equal('strong');
+			Lab.expect(strong[0].localName).to.equal('strong');
+			Lab.expect(strong[0].prefix).to.equal(null);
 
 			body = strong[0].parentNode;
 			bcn  = body.childNodes;
-			assert.equal(body.nodeName, 'body');
+			Lab.expect(body.nodeName).to.equal('body');
 
 			//  childNodes should be a 'live' NodeList instance, which means it will adapt to changes
-			assert.equal(bcn.length, 8);
-			assert.equal(body.childNodes.length, 8);
+			Lab.expect(bcn.length).to.equal(8);
+			Lab.expect(body.childNodes.length).to.equal(8);
 
 			//  insert a last, insert b before it
 			//  <body>....<b/><a/>
-			assert.equal(body.appendChild(a), a);
-			assert.equal(body.insertBefore(b, a), b);
-			assert.equal(bcn.length, 10);
-			assert.equal(body.childNodes.length, 10);
+			Lab.expect(body.appendChild(a)).to.equal(a);
+			Lab.expect(body.insertBefore(b, a)).to.equal(b);
+			Lab.expect(bcn.length).to.equal(10);
+			Lab.expect(body.childNodes.length).to.equal(10);
 
-			assert.equal(a.parentNode, body);
-			assert.equal(b.parentNode, body);
-			assert.equal(a.previousSibling, b);
+			Lab.expect(a.parentNode).to.equal(body);
+			Lab.expect(b.parentNode).to.equal(body);
+			Lab.expect(a.previousSibling).to.equal(b);
 			c = b.previousSibling;
-			assert.equal(b.nextSibling, a);
-			assert.equal(a.nextSibling, null);
+			Lab.expect(b.nextSibling).to.equal(a);
+			Lab.expect(a.nextSibling).to.equal(null);
 
 			//  move a before b
 			//  <body>....<a/><b/>
-			assert.equal(body.insertBefore(a, b), a);
-			assert.equal(a.parentNode, body);
-			assert.equal(b.parentNode, body);
-			assert.equal(a.previousSibling, c);
-			assert.equal(b.previousSibling, a);
-			assert.equal(a.nextSibling, b);
-			assert.equal(b.nextSibling, null);
+			Lab.expect(body.insertBefore(a, b)).to.equal(a);
+			Lab.expect(a.parentNode).to.equal(body);
+			Lab.expect(b.parentNode).to.equal(body);
+			Lab.expect(a.previousSibling).to.equal(c);
+			Lab.expect(b.previousSibling).to.equal(a);
+			Lab.expect(a.nextSibling).to.equal(b);
+			Lab.expect(b.nextSibling).to.equal(null);
 
-			assert.equal(body.removeChild(a), a);
-			assert.equal(body.childNodes.length, 9);
+			Lab.expect(body.removeChild(a)).to.equal(a);
+			Lab.expect(body.childNodes.length).to.equal(9);
 
-			assert.equal(body.removeChild(b), b);
-			assert.equal(body.childNodes.length, 8);
+			Lab.expect(body.removeChild(b)).to.equal(b);
+			Lab.expect(body.childNodes.length).to.equal(8);
 
-			assert.equal(body.removeChild(c), c);
-			assert.equal(body.childNodes.length, 7);
+			Lab.expect(body.removeChild(c)).to.equal(c);
+			Lab.expect(body.childNodes.length).to.equal(7);
 
 			done();
 		});
 	});
 
-	test('Attribute', function(done){
-		var dom = new DOMDocument(),
-			time = process.hrtime(),
+
+	lab.test('Attribute', function(done){
+		var time = process.hrtime(),
 			lang, head, i;
 
-		dom.load(__dirname + '/test.xml', function(error, document){
-//			console.log(elapsed(time));
+		new DOMDocument().load(__dirname + '/test.xml', function(error, document){
 
 			//  lookups on the element.attributes property
-			assert.equal(document.documentElement.attributes.length, 1);
+			Lab.expect(document.documentElement.attributes.length).to.equal(1);
 			lang = document.documentElement.attributes.item(0);
-			assert.equal(lang.name, 'lang');
-			assert.equal(lang.value, 'en');
-			assert.equal(lang.ownerElement.nodeName, 'html');
+			Lab.expect(lang.name).to.equal('lang');
+			Lab.expect(lang.value).to.equal('en');
+			Lab.expect(lang.ownerElement.nodeName).to.equal('html');
 
 			//  lookups on the element itself
-			assert.equal(document.documentElement.getAttribute('lang'), 'en');
+			Lab.expect(document.documentElement.getAttribute('lang')).to.equal('en');
 
 			//  removal (we cannot let mocha compare the result to lang, so we do it ourselves and see it it resolves to true)
-			assert.equal(document.documentElement.attributes.removeNamedItem('lang') === lang, true);
-			assert.equal(document.documentElement.attributes.length, 0);
+			Lab.expect(document.documentElement.attributes.removeNamedItem('lang') === lang).to.equal(true);
+			Lab.expect(document.documentElement.attributes.length).to.equal(0);
 
 			//  removing it again would result in null as it no longer exists
-			assert.equal(document.documentElement.attributes.removeNamedItem('lang'), null);
+			Lab.expect(document.documentElement.attributes.removeNamedItem('lang')).to.equal(null);
 
 			//  manipulating
-			assert.equal(document.documentElement.setAttribute('lang', 'nl'));
-			assert.equal(document.documentElement.attributes.length, 1);
-			assert.equal(document.documentElement.getAttribute('lang'), 'nl');
+			document.documentElement.setAttribute('lang', 'nl');
+			Lab.expect(document.documentElement.attributes.length).to.equal(1);
+			Lab.expect(document.documentElement.getAttribute('lang')).to.equal('nl');
 
 			for (i = 0; i < 10; ++i)
 			{
-				assert.equal(document.documentElement.setAttribute('test' + i, i));
-				assert.equal(document.documentElement.attributes.length, 2 + i);
-				assert.equal(document.documentElement.getAttribute('test' + i), i);
+				document.documentElement.setAttribute('test' + i, i);
+				Lab.expect(document.documentElement.attributes.length).to.equal(2 + i);
+				Lab.expect(document.documentElement.getAttribute('test' + i)).to.equal(i);
 			}
 
 			for (i = 10; i >= 0; --i)
 			{
 				document.documentElement.removeAttribute('test' + i);
-				assert.equal(document.documentElement.attributes.length, 1 + i);
+				Lab.expect(document.documentElement.attributes.length).to.equal(1 + i);
 			}
 
 			lang = document.documentElement.attributes.item(0);
-			assert.equal(document.documentElement.removeAttributeNode(lang) === lang, true);
-			assert.equal(document.documentElement.attributes.length, 0);
+			Lab.expect(document.documentElement.removeAttributeNode(lang) === lang).to.equal(true);
+			Lab.expect(document.documentElement.attributes.length).to.equal(0);
 
 
 			head = document.getElementsByTagName('head');
@@ -214,19 +208,16 @@ suite('DOM', function(){
 		});
 	});
 
-	test('textContent', function(done){
-		var dom = new DOMDocument('1.0', 'utf-8'),
-			time = process.hrtime();
-
-		dom.loadXML('<root>My Text<child>My Child</child></root>', function(error, document){
+	lab.test('textContent', function(done){
+		new DOMDocument('1.0', 'utf-8').loadXML('<root>My Text<child>My Child</child></root>', function(error, document){
 			var root = document.documentElement,
 				content = 'My TextMy Child',
 				children = root.childNodes.length,
 				i;
 
 			//  initialization tests
-			assert.equal(root.nodeName, 'root');
-			assert.equal(root.textContent, content);
+			Lab.expect(root.nodeName).to.equal('root');
+			Lab.expect(root.textContent).to.equal(content);
 
 			//  add nodes with content
 			for (i = 0; i < 10; ++i)
@@ -234,19 +225,19 @@ suite('DOM', function(){
 				root.appendChild(document.createElement('added')).appendChild(document.createTextNode('Added #' + i));
 				content += 'Added #' + i;
 			}
-			assert.equal(root.textContent, content);
-			assert.equal(root.childNodes.length, children + 10);
+			Lab.expect(root.textContent).to.equal(content);
+			Lab.expect(root.childNodes.length).to.equal(children + 10);
 
 			content = 'empty';
 			root.textContent = content;
-			assert.equal(root.textContent, content);
-			assert.equal(root.childNodes.length, 1);
+			Lab.expect(root.textContent).to.equal(content);
+			Lab.expect(root.childNodes.length).to.equal(1);
 
 			done();
 		});
 	});
 
-	test('wholeText', function(done){
+	lab.test('wholeText', function(done){
 		var dom = new DOMDocument('1.0', 'utf-8'),
 			time = process.hrtime();
 
@@ -261,15 +252,15 @@ suite('DOM', function(){
 				root.appendChild(document.createTextNode('Added #' + i));
 				content += 'Added #' + i;
 			}
-			assert.equal(root.textContent, content);
-			assert.equal(root.childNodes.length, 10);
+			Lab.expect(root.textContent).to.equal(content);
+			Lab.expect(root.childNodes.length).to.equal(10);
 
 			for (i = 0; i < root.childNodes.length; ++i)
-				assert.equal(root.childNodes[i].wholeText, content);
+				Lab.expect(root.childNodes[i].wholeText).to.equal(content);
 
 			root.childNodes[4].replaceWholeText('trololo');
-			assert.equal(root.childNodes.length, 1);
-			assert.equal(root.textContent, 'trololo');
+			Lab.expect(root.childNodes.length).to.equal(1);
+			Lab.expect(root.textContent).to.equal('trololo');
 
 			done();
 		});
