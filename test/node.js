a87c5c0e215591612adc7d636dfd7dc339534b68
Rogier Spieker <rogier@konfirm.net>
2014-10-16 07:20:43 +0200

    Added test for Node methods: cloneNode (both shallow and deep), normalize, isSupported (always false, as per todo), compareDocumentPosition. Fixed DOCUMENT_POSITION_PRECEDING flaw

--- test/node.js
+++ test/node.js
@@ -107,12 +107,116 @@ lab.experiment('Node', function(){
 			done();
 		});
 
-		lab.test('cloneNode - deep', function(done){
+		lab.test('cloneNode - shallow', function(done){
 			var clone = a.documentElement.cloneNode();
 
 			Lab.expect(clone.nodeType).to.eql(a.documentElement.nodeType);
 			Lab.expect(clone.nodeName).to.eql(a.documentElement.nodeName);
 			Lab.expect(clone.childNodes.length).to.equal(0);
+
+			done();
+		});
+
+		lab.test('cloneNode - deep', function(done){
+			var clone = a.documentElement.cloneNode(true);
+
+			Lab.expect(clone.nodeType).to.equal(a.documentElement.nodeType);
+			Lab.expect(clone.nodeName).to.equal(a.documentElement.nodeName);
+			Lab.expect(clone.childNodes.length).to.equal(2);
+			Lab.expect(clone.firstChild.nodeName).to.equal(a.documentElement.firstChild.nodeName);
+			Lab.expect(clone.lastChild.nodeName).to.equal(a.documentElement.lastChild.nodeName);
+
+			done();
+		});
+
+		lab.test('normalize', function(done){
+			var node = a.createElement('normalize'),
+				text1 = node.appendChild(a.createTextNode('and ')),
+				text2 = node.appendChild(a.createTextNode('so ')),
+				text3 = node.appendChild(a.createTextNode('it ')),
+				text4 = node.appendChild(a.createTextNode('begins'));
+
+			Lab.expect(node.childNodes.length).to.equal(4);
+			Lab.expect(node.firstChild.nodeValue).to.equal('and ');
+			Lab.expect(node.textContent).to.equal('and so it begins');
+
+			Lab.expect(node.normalize()).to.equal(undefined);  //  no return value
+
+			Lab.expect(node.textContent).to.equal('and so it begins');
+			Lab.expect(node.firstChild.nodeValue).to.equal('and so it begins');
+			Lab.expect(node.childNodes.length).to.equal(1);
+
+			done();
+		});
+
+		lab.test('isSupported', function(done){
+			//  as this is a todo, we only check for false
+
+			Lab.expect(a.documentElement.isSupported('core', 1)).to.equal(false);
+			Lab.expect(a.documentElement.isSupported('core', 2)).to.equal(false);
+			Lab.expect(a.documentElement.isSupported('core', 3)).to.equal(false);
+
+			done();
+		});
+
+		lab.test('compareDocumentPosition', function(done){
+			var offset = b.documentElement.firstChild,
+				disco = b.createElement('disco');
+
+			//  test constants
+			Lab.expect(Node.DOCUMENT_POSITION_DISCONNECTED).to.equal(1);
+			Lab.expect(Node.DOCUMENT_POSITION_PRECEDING).to.equal(2);
+			Lab.expect(Node.DOCUMENT_POSITION_FOLLOWING).to.equal(4);
+			Lab.expect(Node.DOCUMENT_POSITION_CONTAINS).to.equal(8);
+			Lab.expect(Node.DOCUMENT_POSITION_CONTAINED_BY).to.equal(16);
+			Lab.expect(Node.DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC).to.equal(32);
+
+
+			//  same node
+			Lab.expect(
+				offset.compareDocumentPosition(offset)
+			).to.equal(0);
+
+			//  disconnected
+			Lab.expect(
+				offset.compareDocumentPosition(disco) & Node.DOCUMENT_POSITION_DISCONNECTED
+			).to.equal(Node.DOCUMENT_POSITION_DISCONNECTED);
+
+			//  nested preceding (also contains)
+			Lab.expect(
+				offset.parentNode.compareDocumentPosition(offset) & Node.DOCUMENT_POSITION_PRECEDING
+			).to.equal(Node.DOCUMENT_POSITION_PRECEDING);
+			Lab.expect(
+				offset.parentNode.compareDocumentPosition(offset) & Node.DOCUMENT_POSITION_CONTAINS
+			).to.equal(Node.DOCUMENT_POSITION_CONTAINS);
+
+			//  nested following (also contained by)
+			Lab.expect(
+				offset.compareDocumentPosition(offset.parentNode) & Node.DOCUMENT_POSITION_FOLLOWING
+			).to.equal(Node.DOCUMENT_POSITION_FOLLOWING);
+			Lab.expect(
+				offset.compareDocumentPosition(offset.parentNode) & Node.DOCUMENT_POSITION_CONTAINED_BY
+			).to.equal(Node.DOCUMENT_POSITION_CONTAINED_BY);
+
+			//  direct sibling preceding
+			Lab.expect(
+				offset.compareDocumentPosition(offset.nextSibling) & Node.DOCUMENT_POSITION_FOLLOWING
+			).to.equal(Node.DOCUMENT_POSITION_FOLLOWING);
+
+			//  direct sibling following
+			Lab.expect(
+				offset.nextSibling.compareDocumentPosition(offset) & Node.DOCUMENT_POSITION_PRECEDING
+			).to.equal(Node.DOCUMENT_POSITION_PRECEDING);
+
+			//  descended sibling preceding
+			Lab.expect(
+				offset.compareDocumentPosition(offset.nextSibling.firstChild) & Node.DOCUMENT_POSITION_FOLLOWING
+			).to.equal(Node.DOCUMENT_POSITION_FOLLOWING);
+
+			//  descended sibling following
+			Lab.expect(
+				offset.nextSibling.firstChild.compareDocumentPosition(offset) & Node.DOCUMENT_POSITION_PRECEDING
+			).to.equal(Node.DOCUMENT_POSITION_PRECEDING);
 
 			done();
 		});
